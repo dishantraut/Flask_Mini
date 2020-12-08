c5aa255580a12c832e5f1c7e9e92e7430d1da833
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-12-08 12:34:51 +0530

    Redirect & Errors

---
+++ 10_01_try.py
@@ -0,0 +1,25 @@
+"""
+In the following example, the redirect() function is used to display
+the login page again when a login attempt fails.
+"""
+from flask import Flask, redirect, url_for, render_template, request
+# Initialize the Flask application
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('log_in.html')
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login_try():
+   if request.method == 'POST' and request.form['username'] == 'admin' :
+      return redirect(url_for('success'))
+   else:
+      return redirect(url_for('index'))
+
+@app.route('/success')
+def success():
+   return '<h1>Logged In Successfully</h1>'
+
+if __name__ == '__main__':
+   app.run(debug = True)
