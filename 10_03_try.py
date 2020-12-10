c65f8de753818512f94e5726ed5db9930b92186a
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-12-10 11:49:24 +0530

    Redirect & Errors

---
+++ 10_03_try.py
@@ -0,0 +1,23 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def home ():
+    return render_template("10_03_home.html")
+
+@app.route('/login')
+def login():
+    return render_template("10_03_login.html");
+
+@app.route('/validate', methods = ["POST"])
+def validate():
+    if request.method == 'POST' and request.form['pass'] == 'drr':
+        return redirect(url_for("success"))
+    return redirect(url_for("login"))
+
+@app.route('/success')
+def success():
+    return "logged in successfully"
+
+if __name__ == '__main__':
+    app.run(debug = True)
