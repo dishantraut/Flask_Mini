c6cb6c2fc40f07034b9aa7c678ff3e2dfa99ce3c
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-12-11 16:04:37 +0530

    Blueprint 02

---
+++ 20_02_try.py
@@ -0,0 +1,24 @@
+"""
+# FOLDER STRUCTURE
+/app
+    /views
+        __init__.py
+        E_B.py
+
+    20_02_try.py
+
+# For more simplified and detailed version
+
+visit:-
+    https://realpython.com/flask-blueprint/
+
+"""
+from flask import Flask
+from views.E_B import example_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(example_blueprint)
+
+if __name__ == '__main__':
+    app.run(debug=True)
