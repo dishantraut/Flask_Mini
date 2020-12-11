5bac72b8738e6346608a18a7a877ec1e296f7544
dishantraut <63045109+dishantraut@users.noreply.github.com>
2020-12-11 16:49:59 +0530

    WTF Forms

---
+++ 13_01_model.py
@@ -0,0 +1,10 @@
+# model.py
+from wtforms import SubmitField, BooleanField, StringField,PasswordField, validators
+from flask_wtf import FlaskForm
+class RegForm(FlaskForm):
+  name_first = StringField('First Name',[validators.DataRequired()])
+  name_last = StringField('Last Name',[validators.DataRequired()])
+  email = StringField('Email Address', [validators.DataRequired(),validators.Email(), validators.Length(min=6, max=35)])
+  password = PasswordField('New Password', [validators.DataRequired(),validators.EqualTo('confirm',message='Passwords must match')])
+  confirm = PasswordField('Repeat Password')
+  submit = SubmitField('Submit')
